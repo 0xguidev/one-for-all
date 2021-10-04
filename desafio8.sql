51b0590ad9d403957cf90fc18d26d15e64bef918
0xguidev <guilhermeviniciustrabalho@gmail.com>
2021-10-04 20:47:03 -0300

    desafio

---
+++ desafio8.sql
@@ -0,0 +1,12 @@
+ DELIMITER $$
+ CREATE TRIGGER trigger_usuario_delete
+ BEFORE DELETE ON usuarios
+ FOR EACH ROW
+
+ BEGIN
+    delete from seguindo where id_usuario = OLD.id_usuario;
+    delete from historicos where id_usuario = OLD.id_usuario;
+ END $$
+DELIMITER ;
+
+
